ffbe9a00c709563f6ce74f9ddf47b3615973a37b
wick3dr0se <wick3dr0se@protonmail.com>
2022-12-06 15:29:14 -0600

    add 256 color support

--- fm.sh
+++ fm.sh
@@ -1,65 +1,82 @@
 #!/bin/bash
 
 # terminal window cells
-term_size(){ read -r LINES COLUMNS < <(stty size) ;}; term_size
+term_size(){ IFS='[;' read -sp $'\e7\e[9999;9999H\e[6n\e8' -d R -rs _ LINES _ ;}; term_size
 
-# trap window resize
-trap 'term_size' SIGWINCH
-
-# save cursor position 7 -
-# save screen ?47h
-# hide cursor ?25l -
-# set alternative screen buffer ?1049h -
-# disable line wrapping ?7l
-printf '\e7\e[?25l\e[?1049h'
+# save cursor: position 7 -
+# save screen: ?47h
+# hide cursor: ?25l -
+# set alternative screen buffer: ?1049h -
+# disable line wrapping: ?7l
+printf '\e7\e[?25l'
 
 # wipe terminal screen
 clear(){ printf '\e[2J' ;}
 
-# restore cursor position 8 -
-# show cursor ?25h -
-# restore main screen buffer ?1049 -
-# restore screen ?47l
-# enable line wrapoing ?7h
-end(){ printf '\e8\e[?25h\e[?1049l\e8'&& exit ;}
+# restore cursor position: 8 -
+# show cursor: ?25h -
+# restore main screen buffer: ?1049l -
+# restore screen: ?47l
+# enable line wrapoing: ?7h
+end(){ printf '\e[?25h\e8'&& exit ;}
 
-((rows=LINES-1))
-draw(){
-  unset files
+declare -A files
+((rows=LINES-1)) # limit screen size for status bar
+draw_files(){
+  unset files # reset file array
 
+  # inital array index
   y="$rows"
-  [[ $PWD == / ]] && PWD=
-  for fp in "$PWD"/*; do
-    
-    f="${fp##*/}" color=0
 
+  [[ $PWD == / ]] && PWD= # if root, hide /
+  for fp in "$PWD"/*; do
+   
+    f="${fp##*/}" # strip full file path
     if [[ -h $f ]]; then
       # symbolic link
-      color=2 # green
+      # 9: seafoam  5: cyan
+      [[ $TERM =~ 256 ]]&& f="38;5;42m$f" ||
+        f="35m$f"
+    elif [[ -f $f && -x $f || $f == *'.sh' ]]; then
+      # executable
+      # 210: red-orange  1: red
+      [[ $TERM =~ 256 ]]&& f="38;5;210m$f" ||
+        f="32m$f"
     elif [[ -d $f ]]; then
       # directory
-      color=4
-    elif [[ -f $f ]]; then
-      # regular file
-
-      [[ $f == *.sh || -x $f ]]&& color=1 # executable
+      # 147: blurple  # 4: blue
+      [[ $TERM =~ 256 ]]&& f="38;5;147m$f" ||
+        f="34m$f"
+    else # regular file or anything else
+      # 248: grey  # 7;2: dim white
+      [[ $TERM =~ 256 ]]&& f="38;5;248m$f" ||
+        f="37;2m$f"
     fi
 
-    files[$y]="$f"
+    # draw the files
+    printf '\e[%dH\e[%s\e[m' "$y" "$f"
 
-    printf '\e[%dH\e[40;1;3%dm%s\e[m' "$y" "$color" "$f"
+    # store files into an indexed (associative) array
+    files["$y"]="${f#[0-9]*m}"
 
+    # set highlighted color to file color
+    hover["$y"]="${f%%${files[$cursor]}}"
+
+    # iterate the array per file
     ((y--))
+    #(( y == 0 ))&& y="$rows"
   done
-}; draw
+}; draw_files
 
-hud(){
-  printf '\e[%dE\e[2K\e[44m%s\e[m %s: %b' "$LINES" \
+hud(){ # status bar
+  # move down to start of $LINES: E
+  # clear line: 2K
+  printf '\e[%dE\e[2K\e[1;44m%s\e[m %s: %b' "$LINES" \
     'fm' '[←]back [→]open [q]uit' "$status"
 }; hud
 
-draw=1
-cursor="$rows"
+draw=1 # enable draw
+#cursor="$rows" # start cursor above status bar
 
 while read -rsn1; do
   [[ $REPLY == $'\e' ]]&& read -rsn2 # trap keys
@@ -81,7 +98,7 @@ while read -rsn1; do
     F) # create files
       printf '\n%s' 'New file name: '
       read -r file
-      [[ file ]]&&{
+      [[ $file ]]&&{
         >"$file"
 
         # update hud status
@@ -89,7 +106,7 @@ while read -rsn1; do
       }
       clear
     ;;
-    H|B|\[D) cd ../&& clear&& draw;;
+    H|B|\[D) cd ../&& clear&& draw_files;;
     J|\[B) ((cursor++));; # move down
     K|\[A) ((cursor--));; # move up
     X|\[3) # delete
@@ -114,12 +131,13 @@ while read -rsn1; do
       if [[ -d ${files[$cursor]} ]]; then
         cd "${files[$cursor]}"
         # redraw the new files 
-        clear&& draw
+        clear&& draw_files
       
       elif [[ -e ${files[$cursor]} ]]; then
         draw=0 # disable draw
-        # print file to main buffer for scroll
-        printf '\e[?1049l\e[2J'
+        # print file to main buffer
+        #printf '\e[?1049l\e[2J\r'
+
         cat "${files[$cursor]}"
         
         hud
@@ -145,15 +163,21 @@ while read -rsn1; do
   if (( cursor >= LINES )); then
     ((cursor=cursorMin+1))
   elif (( cursor <= cursorMin )); then
-    cursor="$rows"
+   cursor="$rows"
   fi
 
   (( draw ))&&{
-    draw
+    draw_files
 
-    printf '\e[%dH\e[44m%s\e[m' \
-      "$cursor" "${files[$cursor]}"
+    printf '\e[%dH\e[%s%s\e[m' \
+      "$cursor" "${hover[$cursor]/#3/4}" \
+      "${files[$cursor]}"
     
     hud
   }
 done
+
+# trap interrupt and abort signals
+trap '' 2 9
+# trap window resize
+trap 'term_size; clear; draw_files; hud' 28
